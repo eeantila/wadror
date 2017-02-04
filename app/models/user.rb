6518e49aba5d87b4e1d5cd4e739740c0f0abbd1d
Eero Antila <ee.antila@gmail.com>
2017-02-04 17:30:57 +0200

    V3T10 ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,14 @@ class User < ActiveRecord::Base
 
   has_many :ratings
   has_many :beers, through: :ratings
+  has_many :memberships
+  has_many :beer_clubs, through: :memberships
+
+  has_secure_password
 
   validates :username, uniqueness: true,
                        length: { minimum: 3,
                        maximum: 30 }
+  validates :password, :format => {:with => /\A(?=.*[A-Z])(?=.*[0-9]).{4,}\z/, message: "Must be at least 4 characters and include at least one number and one uppercase letter."}
 
 end
