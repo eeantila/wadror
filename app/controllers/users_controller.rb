0b07b6fe0aea4ab34c30e2e290ee7ec914d1f8ba
Eero Antila <ee.antila@gmail.com>
2017-02-05 17:01:08 +0200

    Viikko 3 valmis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,13 +39,13 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
-  def update
+    def update
     respond_to do |format|
-      if @user.update(user_params)
+      if user_params[:username].nil? and @user == current_user and @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -54,11 +54,12 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    @user.destroy if current_user == @user
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
+    session[:user_id] = nil
   end
 
   private
